c28d7ddf6d021d8e83658f7a283d5fcd90a17c3f
Joe <joseph.fabel@gmail.com>
2023-11-14 19:41:13 -0800

    Label ingredients

--- components/SelectionDisplay.js
+++ components/SelectionDisplay.js
@@ -1,12 +1,15 @@
 import React, {useState, useEffect} from 'react';
 
-import {Text, View, FlatList} from 'react-native';
+import {Text, View, FlatList, Pressable} from 'react-native';
 import { Styles } from '../styles/Styles';
-import {getKey, removeKey, RECIPE_KEY} from '../data/Storage';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPrint } from '@fortawesome/free-solid-svg-icons';
 
 function SelectionDisplay({recipeQuantities, availableRecipes, refresh}) {
     const [headers, setHeaders] = useState([]);
     const [ingredientList, setIngredientList] = useState([]);
+
+    let oldLabel = '';
 
     useEffect(() => {
         let theHeaders = [];        
@@ -28,23 +31,57 @@ function SelectionDisplay({recipeQuantities, availableRecipes, refresh}) {
             {                
                 headers.map((item, index) => {
                     return (
-                        <Text style={Styles.text} key={index}>{item.title} - {item.quantity}</Text>
+                        <Text style={Styles.text} key={index}>{item.quantity}x {item.title}</Text>
                     )
                 })
             }
 
             <View style={{...Styles.divider, width: '100%'}} />
-            <FlatList
-                data={ingredientList}
-                style={{}}
-                renderItem={({item}) => {
-                    return (
-                        <Text style={Styles.text}>{item.units}x {item.size} {item.name} ({item.quantity} total)</Text>
-                    );
-                }}
-            />
+
+            <View>
+                <FlatList
+                    data={ingredientList}
+                    style={{}}
+                    renderItem={({item, index}) => {
+                        const showLabel = oldLabel != item.type || index == 0;
+
+                        if (showLabel) {
+                            oldLabel = item.type;
+                            console.log(oldLabel);                        
+                        }
+
+                        return (
+                            <>
+                                {showLabel && <Text style={{...Styles.text, fontWeight: 'bold', marginVertical: 1}}>{item.type}</Text>}
+                                <Text style={Styles.text}>  {item.units}x {item.size} {item.name} ({item.quantity} total)</Text>
+                            </>
+                        );
+                    }}
+                />
+            </View>
+            
+            {
+                ingredientList.length > 0 &&
+                    <>
+                        <View style={{...Styles.divider, width: '100%'}} />
+                        <Text style={Styles.text}>Total: ${calculateTotal()}</ Text>
+                        <Pressable>
+                            <FontAwesomeIcon icon={faPrint} />
+                        </Pressable>
+                    </>
+            }
         </View>
     );
+
+    function calculateTotal() {
+        let total = 0;
+        
+        ingredientList.forEach((item) => {
+            total += item.units * item.price;
+        })
+
+        return total.toFixed(2);
+    }
 
     function getIngredientList() {
         let theList = [];
@@ -55,17 +92,13 @@ function SelectionDisplay({recipeQuantities, availableRecipes, refresh}) {
                 // Add the ingredients to the list
                 recipe.ingredients.forEach((ingredient) => {
                     let foundIndex = theList.findIndex((element) => element.name == ingredient.name);
-
-                    console.log(foundIndex);
-                    if (/*theList.find((value) => value.name == ingredient.name)*/foundIndex >= 0) {
+                    if (foundIndex >= 0) {
                         // update the quantity
-                        console.log(ingredient.name + " is already in the list");
                         theList[foundIndex].quantity += ingredient.quantity * recipeQuantities[index];
 
                     } else {
                         // add item to the list
-                        console.log("adding " + ingredient.name + " to the list!");
-                        theList.push({'name': ingredient.name, 'quantity': ingredient.quantity * recipeQuantities[index], 'size': ingredient.size});
+                        theList.push({'name': ingredient.name, 'quantity': ingredient.quantity * recipeQuantities[index], 'size': ingredient.size, 'price': ingredient.price, 'type': ingredient.type});
                     }
 
                     console.log("ingredient list:");
@@ -89,6 +122,9 @@ function SelectionDisplay({recipeQuantities, availableRecipes, refresh}) {
             item.units = neededUnits;
             
         });
+
+        // sort the list by category
+        theList.sort((a, b) => a.type.localeCompare(b.type));
 
         return theList;
         // return availableRecipes[0]?.ingredients;
